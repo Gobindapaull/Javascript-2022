6262889238d7009326d0305f0d7965826c06badf
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-08-22 09:12:48 +0530

    Create function&setTimeout.js

---
+++ function&setTimeout.js
@@ -0,0 +1,8 @@
+// function with parameter
+function NFTName(name) {
+    console.log(`NFT Name is : ${name}`);
+}
+NFTName('HardWork');
+
+// setTimeout function
+setTimeout(() => console.log('delayed for 3 seconds'), 3000);
