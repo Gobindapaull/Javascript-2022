179216a18c8d7c058422681cf4a07ab4cf9a0ebb
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-09-30 14:19:36 +0530

    Update forLoop.js

--- forLoop.js
+++ forLoop.js
@@ -1,10 +1,15 @@
 const arr = [ 14, "hi", 15, "last" ];
 
+// for Loop
+for ( let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+// alternative for 
 for (const item of arr) {
   console.log(item)
 }
 
-// alternative method
+// forEach Loop
 
 arr.forEach((item) => {
   console.log(item)
