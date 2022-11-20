c561b19bf6a3e83cc7ae0b437d2eb85e3a07b814
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-11-20 10:12:43 +0530

    Hypotenuse calculate

--- math.js
+++ math.js
@@ -19,3 +19,11 @@ console.log(myNumberMininum);
 console.log(myNumberMaximum);
 console.log(myNumberPI);
 console.log(myNumberE);
+
+
+// right angle triangle
+
+let a = 3;
+let b = 4;
+let c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+console.log(c);
