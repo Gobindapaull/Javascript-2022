d1de766348a5e99fb7a2848527a0fdbb815d28e4
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-11-20 10:05:39 +0530

    Create math.js

---
+++ math.js
@@ -0,0 +1,21 @@
+let myNumberRound = Math.round(3.5);
+let myNumberFloor = Math.floor(3.5);
+let myNumberCeil = Math.ceil(3.5);
+let myNumberPower = Math.pow(3,5);
+let myNumberSquareRoot= Math.sqrt(81);
+let myNumberAbsolute = Math.abs(-3.5);
+let myNumberMininum = Math.min(1, 5, 2, 4, 9);
+let myNumberMaximum = Math.max(2, 9, 99, 101 , 0, -2);
+let myNumberPI = Math.PI;
+let myNumberE = Math.E;
+
+console.log(myNumberRound);
+console.log(myNumberFloor);
+console.log(myNumberCeil);
+console.log(myNumberPower);
+console.log(myNumberSquareRoot);
+console.log(myNumberAbsolute);
+console.log(myNumberMininum);
+console.log(myNumberMaximum);
+console.log(myNumberPI);
+console.log(myNumberE);
