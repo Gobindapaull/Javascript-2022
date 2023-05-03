1c71d15b2614bbfe61834b4bcd5e3dfd3bd3c548
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-03 11:21:25 +0530

    Create day1.js

---
+++ src/day1.js
@@ -0,0 +1,17 @@
+console.log(typeof "day")
+// string
+
+let myVariable = "string data type"
+// string
+
+console.log(1234*101)
+// 124634
+
+let myNum = 1e18
+console.log(typeof myNum)
+// number
+
+let stringNumber = myVariable + "\n" + myNum
+console.log(stringNumber)
+// string
+console.log(typeof stringNumber)
