2ba235aa84493386a8125566e9aa2e158f04bdaf
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-07 08:47:18 +0530

    Create dom.js

---
+++ concepts/dom.js
@@ -0,0 +1,34 @@
+let element;
+
+id = document.getElementById("child");
+
+className = document.getElementsByClassName("parent");
+
+doctype = document.doctype;
+
+element = document.all[5];
+
+links = document.links;
+
+head = document.head;
+
+body = document.body;
+
+images = document.images;
+
+forms = document.forms;
+
+domain = document.domain;
+
+baseUri = document.baseURI;
+
+console.log(element);
+console.log(body);
+console.log(links);
+console.log(images);
+console.log(forms);
+console.log(doctype);
+console.log(domain);
+console.log(baseUri);
+console.log(id);
+console.log(className);
