7a2b788fd4fe0f2c70fd94690ecd5731e41e3659
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-23 02:36:31 +0530

    Create string.js

---
+++ yt/string.js
@@ -0,0 +1,5 @@
+let strObjectPrimitive = new String('string name')
+let strPrimitive = 'string name'
+
+console.log(strObjectPrimitive == strPrimitive)
+console.log(strObjectPrimitive === strPrimitive)
