f8a2bbb67d9756ffa210e4f69cba4f8ea6dd7c72
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-23 02:03:37 +0530

    Update objMerge.js

--- yt/objMerge.js
+++ yt/objMerge.js
@@ -9,3 +9,16 @@ let obj2 = {
 // Merging Objects
 let resultObj = Object.assign(obj1, obj2)
 console.log(resultObj)
+
+let obj3 = {
+    name: "john",
+    role: "owner",
+}
+
+let arr = [{score1: 99, score2: 97, score3: 93}]
+
+let newArrObj = Object.assign({}, ...arr)
+console.log(newArrObj)
+
+let newObj = Object.assign({}, obj3)
+console.log(newObj)
