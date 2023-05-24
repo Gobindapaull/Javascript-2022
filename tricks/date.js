451ab1b424caea42274f238a271a1a8d654ff50c
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-24 13:57:56 +0530

    Create date.js

---
+++ tricks/date.js
@@ -0,0 +1,8 @@
+let date = new Date()
+
+// Infinite loops in Javascript
+while(true) {
+    console.log(typeof date) // object
+    console.log(date) // 2023-05-24T08:25:39.755Z
+    console.log(date instanceof Date) // true
+}
