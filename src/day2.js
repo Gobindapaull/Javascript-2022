88e8ec38d3fbfe4b702a5eb205b88005ae472671
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-19 05:45:35 +0530

    Create day2.js

---
+++ src/day2.js
@@ -0,0 +1,28 @@
+
+console.log('node js :)')
+
+// array and push method
+const crypto = ['ETH']
+console.log(crypto)
+console.log(crypto[0]) // zero index
+
+crypto.push('BNB')
+console.log(crypto)
+
+// let 
+let a = 10
+a = 20 // prefer
+console.log(a)
+
+// const
+const b = 20
+// b = 30 // TypeError: Assignment to constant variable
+console.log(b)
+
+// object
+const obj = {
+    a: 10,
+    c: true
+}
+console.log(obj)
+console.log(obj.c)
