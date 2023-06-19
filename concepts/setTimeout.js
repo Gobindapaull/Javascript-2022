5b2053955d946fa70494212f3710e9b2bfed4450
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-19 06:00:51 +0530

    Update setTimeout.js

--- concepts/setTimeout.js
+++ concepts/setTimeout.js
@@ -1,7 +1,8 @@
-console.log('wait for 3 seconds...')
+console.log('wait for 5 seconds...')
 
 function Wait() {
     console.log('withdraw successful :)')
+    console.log('https://bscscan.com/address/0x0000000000000000000000000000000000000000')
 }
 
-setTimeout(Wait, 3000)
+setTimeout(Wait, 5000)
