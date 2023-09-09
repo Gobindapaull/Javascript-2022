8bea4179364e6cc97aa6e82056f7d1d73b5db933
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-09-09 09:36:59 +0530

    Create console.js

---
+++ src/console.js
@@ -0,0 +1,12 @@
+let programming = "JS"
+let duration = "1 years"
+let cost = "$300"
+console.log(programming, duration, cost)
+
+
+const connected = true
+if(connected) {
+    console.log('Metamask is connected')
+} else {
+    console.log("Please Install Meatamask")
+}
