7e0929ea30e5337f53ddb8755f8bfa55861e176e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-10-01 00:30:15 +0530

    Create random.js

---
+++ concepts/random.js
@@ -0,0 +1,14 @@
+const random = Math.random()
+console.log(random)
+// always less than 1
+// 0.8549260891610089
+// 0.8549260891610089
+console.log(random * 9)
+// 8.300271884958843
+// 8.108729899882977
+console.log(Math.floor(Math.random() * 5))
+// 2
+// 4
+console.log(Math.floor(Math.random() * 3) + 4)
+// 5
+// 6
