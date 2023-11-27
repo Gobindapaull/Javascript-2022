bc96a1b30a4c712412aae1c16e628987bee91be6
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-11-28 01:51:14 +0530

    Create obj.js

---
+++ src/obj.js
@@ -0,0 +1,21 @@
+const obj = [
+    {
+        "SYMBOL": "USDT",
+        "ADDRESS": "0x55d398326f99059fF775485246999027B3197955",
+
+        "GASLIMIT": "500000",
+        "GAS": "5",
+        "SLIPPAGE": "49"
+    },
+    {
+        "SYMBOL": "USDC",
+        "ADDRESS": "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+
+        "GASLIMIT": "500000",
+        "GAS": "5",
+        "SLIPPAGE": "49"
+    }
+]
+
+console.log(obj[0].ADDRESS)
+console.log(obj[1].ADDRESS)
