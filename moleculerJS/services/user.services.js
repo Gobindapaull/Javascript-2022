afefb58299d74031d18ecaa92cdea5cb67720361
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-04-03 03:42:51 +0530

    Create user.services.js

---
+++ moleculerJS/services/user.services.js
@@ -0,0 +1,25 @@
+import { ServiceBroker } from "moleculer";
+
+const broker = new ServiceBroker();
+
+function generateID() {
+  return Math.floor(Math.random() * 1000) + 1;
+}
+
+const users = [];
+broker.createService({
+  name: "user",
+  actions: {
+    async createUser(ctx) {
+      const { username, email } = ctx.params;
+      const newUser = { id: generateID(), username, email };
+      users.push(newUser);
+      return newUser;
+    },
+    async getUsers(ctx) {
+      return users;
+    },
+  },
+});
+
+export default broker;
