e3e669798861ded13b3fdb9b6e12538678271f85
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-04-18 02:59:27 +0530

    Create index.js

---
+++ chalk/index.js
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+
+console.log(`
+${chalk.bgBlueBright.whiteBright(`
+  BSC AND ETH CLAIMER 2024`)}
+${chalk.cyanBright(`
+  Telegram: 
+  https://t.me/autoboyt
+
+  Youtube
+  https://www.youtube.com/@soltsdev
+  
+  Donate address:
+  ${chalk.bgYellowBright.whiteBright("0x422B0755EABeA90Cc2C5674F8Bba65C861962fdD")}
+`)}
+`)
