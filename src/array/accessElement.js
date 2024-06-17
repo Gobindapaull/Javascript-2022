e0a423d71da15fd69b6f2244cfb09e8c07fb0ee0
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-18 04:58:42 +0530

    Create accessElement.js

---
+++ src/array/accessElement.js
@@ -0,0 +1,8 @@
+const names = [1, 2, 3, 4, 5, 6]
+const addNames = [...names, 7, 8, 9, 10]
+
+
+console.log(names[0]) // access first element
+console.log(names[names.length - 1]) // access last element
+
+console.log(addNames[addNames.length - 1])
