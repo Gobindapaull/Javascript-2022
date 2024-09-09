165963b3c5ca507a5a4753bc3933892162d02523
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-09-09 09:40:06 +0530

    Update index.js

--- LUNC/index.js
+++ LUNC/index.js
@@ -1,4 +1,4 @@
-import { LCDClient, MnemonicKey } from '@terra-money/terra.js';
+import { LCDClient, MnemonicKey, MsgSend } from '@terra-money/terra.js';
 
 const terra = new LCDClient({
     URL: 'https://columbus-lcd.terra.dev',
@@ -7,17 +7,18 @@ const terra = new LCDClient({
 });
 
 const mk = new MnemonicKey({
-    mnemonic: "test test test test test test test test test test test test test test test test test test test test test test test test"
+    mnemonic: ""
 });
 const mnemonic = terra.wallet(mk).key.mnemonic;
 const wallet = terra.wallet(mk);
+const randomWallet = new MnemonicKey().accAddress;
 
 console.log(`============[Wallet INFO]===========`)
 
-console.log(`[Mnemonic]: ${mnemonic}`)
+// console.log(`[Mnemonic]: ${mnemonic}`)
 console.log(`[wallet address]: ${wallet.key.accAddress}`);
 
-console.log(`[Random address]: ${new MnemonicKey().accAddress}`);
+console.log(`[Random address]: ${randomWallet}`);
 
 console.log(`============[Terra classic INFO]===========`)
 
@@ -28,3 +29,28 @@ console.log(terra.config.gasPrices)
 console.log(terra.config.gasAdjustment)
 
 console.log(`============[x]===========`)
+
+const bot = async () => {
+    const balance = await terra.bank.balance(wallet.key.accAddress)
+    console.log(`Total coins: ${balance[1].total}`)
+
+    // create a simple message that moves coin balances
+    const send = new MsgSend(
+        wallet.key.accAddress,
+        randomWallet,
+        { uluna: 20000000 }
+    );
+
+    const tx = await wallet.createAndSignTx({
+        msgs: [send],
+        memo: 'Test from terra.js!',
+    })
+    const txResult = await terra.tx.broadcast(tx);
+    const logs = txResult.raw_log;
+    console.log(`Transaction successfull -> https://finder.station.money/classic/tx/${txResult.txhash}`)
+    console.log(`Logs: ${logs}`)
+}
+
+bot()
+
+
