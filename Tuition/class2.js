324f1e0051a049de218409f7d067febafc9a2ebc
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-22 11:03:43 +0530

    Create class2.js

---
+++ Tuition/class2.js
@@ -0,0 +1,18 @@
+let obj = {
+    name: "abc",
+    age: 2,
+    training: function task() {
+        console.log("fun ..")
+    }
+}
+
+obj["training"]()
+
+
+// primitive type
+// NNBBSSU
+// number, null, boolean, bigint, string, symbol, uint
+
+// Non Primitive type
+// Object, Array
+
