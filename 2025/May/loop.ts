48fbd612a9e7e2d2e9e4f4c3deb00b137fb4820a
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-05-21 15:28:44 +0530

    Create loop.ts

---
+++ 2025/May/loop.ts
@@ -0,0 +1,9 @@
+// loop
+for (let i = 0; i < 5; i++) {
+    console.log(`Index: ${i}`);
+}
+
+// map
+const numbers: number[] = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled); // [ 2, 4, 6, 8, 10 ]
