140b7003e3f07238e6a86cb67e262c894102f283
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-06-05 00:08:12 +0530

    Update index.js

--- 2025/parseFloat/index.js
+++ 2025/parseFloat/index.js
@@ -1,8 +1,7 @@
-
 const min = parseFloat(0.054);
-console.log(typeof min); // number
+console.log(typeof min);
 const max = parseFloat(0.056);
 
 const result = max - min;
-console.log(result, typeof result); // 0.0020000000000000018
-console.log(result.toFixed(6), typeof result.toFixed(6)); // 0.002000
+console.log(result, typeof result); // 0.0020000000000000018 // number
+console.log(result.toFixed(6), typeof result.toFixed(6)); // 0.002000 // string
