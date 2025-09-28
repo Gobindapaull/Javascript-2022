efea4a047cd67a46cd50baf7ff8ddf90f53fb4fa
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-28 07:29:54 +0530

    Create perimeter.js

---
+++ Leetcode/LargestPerimeterTriangle/perimeter.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function (nums) {
+    nums.sort((a, b) => b - a); // Descending order 
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        let a = nums[i];
+        let b = nums[i + 1];
+        let c = nums[i + 2];
+
+        if (b + c > a) {
+            return a + b + c;
+        }
+    }
+    return 0;
+};
+
+// https://leetcode.com/problems/largest-perimeter-triangle/
