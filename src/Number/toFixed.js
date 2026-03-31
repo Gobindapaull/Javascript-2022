b3e2ccddb5c6ab80a7089a4ff263c5a8674add71
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-03-31 10:30:12 +0530

    Create toFixed.js

---
+++ src/Number/toFixed.js
@@ -0,0 +1,7 @@
+const b = 10.48583933
+const c = b.toFixed(6)
+const d = b.toPrecision(6)
+
+console.log(b, typeof(b)) // 10.48583933 number
+console.log(c, typeof(c)) // 10.485839 string
+console.log(d, typeof(d)) // 10.4858 string
