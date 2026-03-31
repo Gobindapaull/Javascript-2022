30d197fc03f1aecde5ba08c0d9bcf41459a0e5ec
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-03-31 10:23:24 +0530

    Create object.js

---
+++ src/array/object.js
@@ -0,0 +1,6 @@
+let a = [];
+
+a[0] = {name: "crypto", price: 200}
+a[1] = {name: "bike", price: 2500}
+
+console.log(a, typeof(a)); // [ { name: 'crypto', price: 200 }, { name: 'bike', price: 2500 } ], object
