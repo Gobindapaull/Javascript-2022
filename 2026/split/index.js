9bca6eec84ade9a3692ffe33c0e1e7612b59c454
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-04-03 10:35:41 +0530

    Create index.js

---
+++ 2026/split/index.js
@@ -0,0 +1,11 @@
+for (let i = 5; i >= 0; i--) {
+    console.log(i);
+}
+// 5 4 3 2 1 0
+
+let inputs = `5
+3`;
+
+let input = inputs.split('\n');
+
+console.log(input, typeof input); // [ '5', '3' ] object
