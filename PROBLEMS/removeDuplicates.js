d46e0abe3a5feed2bf2dede23845a47f5ac9010b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-04-24 21:32:06 +0530

    Update removeDuplicates.js

--- PROBLEMS/removeDuplicates.js
+++ PROBLEMS/removeDuplicates.js
@@ -7,3 +7,10 @@ function removeDuplicates(names) {
 
 console.log(removeDuplicates(name));
 // [ 'aa', 'bb', 'cc', 'dd' ]
+
+
+// Removes duplicates
+const arr = [1, 2, 2, 3, 3, 4, 4, 5, 6]
+
+const unique = [...new Set(arr)]
+console.log(unique)
