c57a828cfb60b29779de1e03d8accf61a8d286f6
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-05-10 01:38:41 +0530

    Create updateWallets.js

---
+++ supabase/database/updateWallets.js
@@ -0,0 +1,15 @@
+const supabase = require("./db");
+
+async function main() {
+    const wallet = "0xCcE092c904928c120669DD1ba1b3A91d2DF30654";
+
+    const { data, error } = await supabase.from("wallets").update({ "balance": 999 }).eq("wallet", wallet).select();
+
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`DATA: ${JSON.stringify(data, null, 2)}`);
+    }
+}
+
+main();
