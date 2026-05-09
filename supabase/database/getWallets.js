ee7801a2ba19109ef924ed7035bd2f1a438c7866
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-05-10 01:31:23 +0530

    Update getWallets.js

--- supabase/database/getWallets.js
+++ supabase/database/getWallets.js
@@ -1,13 +1,13 @@
 const supabase = require("./db");
 
 async function main() {
+    const { data, error } = await supabase.from("wallets").select("*"); // .gt("balance", 0)
 
-    const { data, error } = await supabase
-        .from("wallets")
-        .select("*");
-
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(`data: ${JSON.stringify(data, null, 2)}`);
+    }
 }
 
 main();
