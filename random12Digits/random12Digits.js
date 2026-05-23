575e22a769cce21bfb9b2ac6fe97b68c48667f92
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-05-23 08:42:52 +0530

    Update random12Digits.js

--- random12Digits/random12Digits.js
+++ random12Digits/random12Digits.js
@@ -11,3 +11,7 @@ generate12DigitNumber();// [5, 6, 7, 7, 8, 4, 7, 2, 8, 7, 0, 3]
 const random12Digits = Array.from({ length: 12 }, () => Math.floor(Math.random() * 10)).join("")
 
 console.log(random12Digits); // 767360511690
+
+// OR
+const result = Math.floor(100000000000 + Math.random() * 900000000000);
+console.log(result);
