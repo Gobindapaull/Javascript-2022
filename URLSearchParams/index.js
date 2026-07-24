726e2f5c6b8975354fd5ba7095b9228e7dbc0ecc
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-07-24 07:36:43 +0530

    Update index.js

--- URLSearchParams/index.js
+++ URLSearchParams/index.js
@@ -9,3 +9,7 @@ console.log(query);
 
 const url = `https://google.com?${query}`;
 console.log(url);
+
+
+const url1 = new URL("https://developer.mozilla.org/en-US/docs/Web/API/URL/toString");
+console.log(url1.toString());
